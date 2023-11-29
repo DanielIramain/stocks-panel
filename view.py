82697533697b4faa8ae0de0291a1f13f522c618a
D. Daniel Iramain <danieliramain97@gmail.com>
2023-11-29 10:50:00 -0300

    Boton salir

--- view.py
+++ view.py
@@ -1,10 +1,12 @@
-from tkinter import Tk, ttk
+from tkinter import *
+from tkinter import ttk
 from tkinter import BOTTOM, TOP
 
 from controller import capturar_datos
 from stockPanel import Fundamentos
 
 #GUI
+###Ventana principal de la aplicacion
 root = Tk()
 frame = ttk.Frame(root, padding=100)
 combo = ttk.Combobox(root, state='readonly', 
@@ -24,6 +26,7 @@ ttk.Label(frame, text='Escriba su clave').pack(side='top')
 entrada_api_key = ttk.Entry(frame)
 entrada_api_key.pack(side=TOP)
 
+ttk.Button(frame, text='Salir', command=quit).pack(side=BOTTOM)
 ttk.Button(frame, text='Mostrar datos', command=Fundamentos.obtener_fundamentos).pack(side=BOTTOM)
 ttk.Button(frame, text='Guardar datos', command=capturar_datos).pack(side=BOTTOM)
 combo.place(x=90, y=30)
