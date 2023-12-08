22e23145f6706d35c781243a37a6c89ab1d65656
D. Daniel Iramain <danieliramain97@gmail.com>
2023-12-08 09:35:20 -0300

    refactory

--- sample/view.py
+++ sample/view.py
@@ -1,26 +1,22 @@
-#from tkinter import *
 from tkinter import Tk, ttk
 from tkinter import BOTTOM, TOP
 
-def capturar_datos() -> None:
-    ###Se encarga de capturar los datos mostrados a través de GUI para ser usados en los métodos
-    global simbolo
-    global servicio
-    global API_KEY
-    
-    simbolo = entrada_ticker.get()
-    servicio = combo.get()
-    API_KEY = entrada_api_key.get()
-    
-    print('simbolo: ', simbolo)
-    print('servicio: ', servicio)
-    print('API KEY: ', API_KEY)
-    
-    #return simbolo
-    #return API_KEY
-
 def obtener_fundamentos():
+        ###Se encarga de capturar los datos mostrados a través de GUI para ser usados en los métodos
+        ###Luego llama a las funciones que corresponden
         from main import elegir_funcion, solicitar_informacion
+        global simbolo
+        global servicio
+        global API_KEY
+        
+        simbolo = entrada_ticker.get()
+        servicio = combo.get()
+        API_KEY = entrada_api_key.get()
+        
+        print('simbolo: ', simbolo)
+        print('servicio: ', servicio)
+        print('API KEY: ', API_KEY)
+
         elegir_funcion(servicio)
         solicitar_informacion()
 
@@ -37,17 +33,18 @@ combo = ttk.Combobox(root, state='readonly',
 
 
 frame.grid()
-ttk.Label(frame, text='Escriba un ticker').pack(side='top')
 
+ttk.Label(frame, text='Escriba un ticker').pack(side='top')
 entrada_ticker = ttk.Entry(frame)
 entrada_ticker.pack(side=TOP)
+
 ttk.Label(frame, text='Escriba su clave').pack(side='top')
 entrada_api_key = ttk.Entry(frame)
 entrada_api_key.pack(side=TOP)
 
 ttk.Button(frame, text='Salir', command=quit).pack(side=BOTTOM)
 ttk.Button(frame, text='Mostrar datos', command=obtener_fundamentos).pack(side=BOTTOM)
-ttk.Button(frame, text='Guardar datos', command=capturar_datos).pack(side=BOTTOM)
+
 combo.place(x=90, y=30)
 
 root.mainloop()
